2e8cd05a0b1e9623040c88040b81a5407985fcc2
em1n4k <emin4kblin4ik@gmail.com>
2025-05-13 23:07:49 +0300

    Models: Teacher has been updated

--- src/main/java/controller/StudentServlet.java
+++ src/main/java/controller/StudentServlet.java
@@ -34,6 +34,4 @@ public class StudentServlet extends HttpServlet {
 
     }
 
-
-
 }
--- src/main/java/models/Teacher.java
+++ src/main/java/models/Teacher.java
@@ -1,21 +1,29 @@
 package models;
 
+import java.math.BigDecimal;
+
 public class Teacher {
 
     private long id;
     private String firstName;
     private String lastName;
     private String subject;
-    private float salary;
+    private BigDecimal salary;
+    private String email;
+    private String phoneNumber;
 
-    public Teacher(long id, String firstName, String lastName, String subject, float salary) {
+    public Teacher(long id, String firstName, String lastName, String email, String subject, String phoneNumber, BigDecimal salary) {
 
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
         this.subject = subject;
         this.salary = salary;
     }
+
+    public Teacher () {}
 
     public long getId() {
         return id;
@@ -41,6 +49,22 @@ public class Teacher {
         this.lastName = lastName;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = this.phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getSubject() {
         return subject;
     }
@@ -49,12 +73,18 @@ public class Teacher {
         this.subject = subject;
     }
 
-    public float getSalary() {
+    public BigDecimal getSalary() {
         return salary;
     }
 
-    public void setSalary(float salary) {
+    public void setSalary(BigDecimal salary) {
         this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", subject='" + subject + '\'' + ", salary=" + salary + '}';
     }
 
 
