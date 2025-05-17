4285521b474b78a733518e06d55d58a2e562c445
em1n4k <emin4kblin4ik@gmail.com>
2025-05-17 20:40:25 +0300

    Models: Teacher and Student update

--- src/main/java/controller/StudentServlet.java
+++ src/main/java/controller/StudentServlet.java
@@ -17,8 +17,8 @@ public class StudentServlet extends HttpServlet {
     public void init() throws ServletException {
         // Инициализация DAO и объявление тестовых данных
         studentDAO = new StudentDAO();
-        studentDAO.add(new Student(1, "Материй", "Силоуков", 18 ,"matsil@gmail.com", "42.03.01", null, "+7(999)941-52-52" ));
-        studentDAO.add(new Student(2, "Мустафа", "Ахмедов", 18 ,"mustafa@mail.ru", "42.03.01", null, "+7(999)952-23-24" ));
+        studentDAO.add(new Student(1, "Материй", "Силоуков", "Ахмедов", 18 ,"matsil@gmail.com", "42.03.01", null, "+7(999)941-52-52" ));
+        studentDAO.add(new Student(2, "Мустафа", "Ахмедов", "Сило", 18 ,"mustafa@mail.ru", "42.03.01", null, "+7(999)952-23-24" ));
     }
 
     @Override
